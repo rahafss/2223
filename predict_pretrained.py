0d293af508ada7e74e479f0bddfa0084d4e8d6d8
rahafss <83409737+rahafss@users.noreply.github.com>
2021-05-19 00:12:35 +0200

    Add files via upload

---
+++ predict_pretrained.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Diese Datei sollte nicht verändert werden und wird von uns gestellt und zurückgesetzt.
+
+Skript testet das vortrainierte Modell
+
+
+@author: Maurice Rohr
+"""
+
+from predict import predict_labels
+from wettbewerb import load_references, save_predictions
+
+if __name__ == '__main__':
+
+    ecg_leads,ecg_labels,fs,ecg_names = load_references('../test/') # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
+    
+    predictions = predict_labels(ecg_leads,fs,ecg_names,use_pretrained=True)
+    
+    save_predictions(predictions) # speichert Prädiktion in CSV Datei
