533614a0c6c12046bf2bb2f4b0cbee1cd336b03a
rahafss <83409737+rahafss@users.noreply.github.com>
2021-05-19 00:19:53 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,64 +1,31 @@
 # -*- coding: utf-8 -*-
 """
 
-Skript testet das vortrainierte Modell
+from __future__ import print_function
 
-
-@author: Christoph Hoog Antink, Maurice Rohr
-"""
-
-import csv
-import scipy.io as sio
-import matplotlib.pyplot as plt
+import argparse
 import numpy as np
-from ecgdetectors import Detectors
+import keras
 import os
 
-###Signatur der Methode (Parameter und Anzahl return-Werte) darf nicht verändert werden
-def predict_labels(ecg_leads,fs,ecg_names,use_pretrained=False):
-    '''
-    Parameters
-    ----------
-    model_name : str
-        Dateiname des Models. In Code-Pfad
-    ecg_leads : list of numpy-Arrays
-        EKG-Signale.
-    fs : float
-        Sampling-Frequenz der Signale.
-    ecg_names : list of str
-        eindeutige Bezeichnung für jedes EKG-Signal.
+import load
+import util
 
-    Returns
-    -------
-    predictions : list of tuples
-        ecg_name und eure Diagnose
-    '''
+def predict(data_json, model_path):
+    preproc = util.load(os.path.dirname(model_path))
+    dataset = load.load_dataset(data_json)
+    x, y = preproc.process(*dataset)
 
-#------------------------------------------------------------------------------
-# Euer Code ab hier  
-    model_name = "model.npy"   
-    if use_pretrained:
-        model_name = "model_pretrained.npy"
-    with open(model_name, 'rb') as f:  
-        th_opt = np.load(f)         # Lade simples Model (1 Parameter)
+    model = keras.models.load_model(model_path)
+    probs = model.predict(x, verbose=1)
 
-    detectors = Detectors(fs)        # Initialisierung des QRS-Detektors
+    return probs
 
-    predictions = list()
-    
-    for idx,ecg_lead in enumerate(ecg_leads):
-        r_peaks = detectors.hamilton_detector(ecg_lead)     # Detektion der QRS-Komplexe
-        sdnn = np.std(np.diff(r_peaks)/fs*1000) 
-        if sdnn < th_opt:
-            predictions.append((ecg_names[idx], 'N'))
-        else:
-            predictions.append((ecg_names[idx], 'A'))
-        if ((idx+1) % 100)==0:
-            print(str(idx+1) + "\t Dateien wurden verarbeitet.")
-            
-            
-#------------------------------------------------------------------------------    
-    return predictions # Liste von Tupels im Format (ecg_name,label) - Muss unverändert bleiben!
-                               
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_json", help="path to data json")
+    parser.add_argument("model_path", help="path to model")
+    args = parser.parse_args()
+    probs = predict(args.data_json, args.model_path)
                                
         
